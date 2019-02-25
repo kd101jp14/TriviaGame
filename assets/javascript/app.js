792fbfc894142a56217f746008ff044edb0768fb
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-02-25 03:19:17 -0600

    Added sound effect for when the display switches to the time up/results view.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,27 @@
 $(document).ready(function () {
 
-    var counter = 10;
-    
+    // The user has 90 seconds to answer trivia questions, so counter starts at 90.
+    var counter = 90;
+
+    //The `interval` variable is defined within the restart function.
+    // It is declared globally so other functions may have access to it.
     var interval;
 
     // The user first sees the start screen.
     $("#startScreen").show();
     $("#gameScreen, #timeUpScreen").hide();
 
-    // When the user presses the start button, quiz is displayed. Quiz starts.
+    // When the user presses the start button, quiz is displayed and countdown starts.
+    // (This function is also executed when the user presses the restart button after results are displayed.)
     $(".playbtn").click(function () {
         $("#startScreen, #timeUpScreen").hide();
         $("#gameScreen").show();
-        // $("#hereWeGo")[0].play();
-        counter = 10;
+        $("#hereWeGo")[0].play();
+        counter = 90;
         restart();
     });
 
+    // Instead of waiting for time to run out, the user may see results by pressing the submit button.
     $("#submitButton").click(function () {
         timeUp();
         clearInterval(interval);
@@ -42,6 +47,7 @@ $(document).ready(function () {
     function timeUp() {
         $("#startScreen, #gameScreen").hide();
         $("#timeUpScreen").show();
+        $("#yay")[0].play();
     }
 
 
