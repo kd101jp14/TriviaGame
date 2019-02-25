1dea40e8543a70c48176c2572397466956b03403
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-02-25 00:49:17 -0600

    Created working countdown that dynamically updates and displays on trivia page. Created restart function.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,38 @@
 $(document).ready(function () {
+
+    var counter = 5;
+
+    var interval = setInterval(decreaseSeconds, 1000);
+
+    function decreaseSeconds() {
+        counter--;
+        $(secondsLeft).text("Seconds remaining: " + counter);
+        if (counter === 0) {
+            clearInterval(interval);
+            timeUp();
+        }
+    }
+
+    function restart() {
+        // All choices are cleared/unchecked.
+        $('input[name=choice]').prop('checked', false);
+        // Time starts over.
+        interval;
+    }
+
+    // The user first sees the start screen.
     $("#gameScreen").hide();
 
+    // When the user presses the start button, quiz is displayed. Quiz starts.
     $("#startButton").click(function () {
         $("#startScreen").hide();
         $("#gameScreen").show();
+        // $("#hereWeGo")[0].play();
+        restart();
     });
+
+
+
+
+
 });
